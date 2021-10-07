be07fb459c51dc5669fcd0bd01a6026f4e0814af
Serji Chacko <serji.chacko@avrioc.com>
2021-10-07 16:51:57 +0400

    upfdate

--- terraform_project/modules/vpc/vpc.tf
+++ terraform_project/modules/vpc/vpc.tf
@@ -1,16 +1,12 @@
-provider "aws" {
-  region = var.region
-}
-
-resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "this" {
-  vpc_id     = aws_vpc.this.id
-  cidr_block = "10.0.1.0/24"
-}
-
-data "aws_ssm_parameter" "this" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+# create-vpc.tf
+ 
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = var.vpc_dns_hostnames
+  enable_dns_support   = var.vpc_dns_support
+ 
+  tags = {
+    "Owner" = var.owner
+    "Name"  = "${var.owner}-vpc"
+  }
 }
